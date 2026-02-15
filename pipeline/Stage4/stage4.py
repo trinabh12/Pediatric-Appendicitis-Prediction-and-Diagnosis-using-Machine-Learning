8b3c1a60ff00f23ca80916de40e102d4f7355a57
Trinabh Mitra <mitratrinabh@gmail.com>
2026-02-15 06:40:04 +0530

    some path changes

--- pipeline/Stage4/stage4.py
+++ pipeline/Stage4/stage4.py
@@ -1,12 +1,16 @@
+import os
 from feature_handling import HandleFeatures
 
 PREV_STAGE = "../Stage3"
 DATASET_DIR = "prepared dataset"
 
+TABULAR_DATA = os.path.join(DATASET_DIR, "tabular")
 FILE_NAME = "prepared_data.xlsx"
 INFO_REPORT = "feature_info.json"
 DERIVED_INFO_REPORT = "derived_info.json"
 GROUPED_REPORT = "feature_groups.json"
 
-fe = HandleFeatures(PREV_STAGE, DATASET_DIR, FILE_NAME, INFO_REPORT,DERIVED_INFO_REPORT, GROUPED_REPORT)
+IMAGE_DATA = os.path.join(DATASET_DIR, "image")
+
+fe = HandleFeatures(PREV_STAGE, TABULAR_DATA, FILE_NAME, INFO_REPORT, DERIVED_INFO_REPORT, GROUPED_REPORT)
 fe.save_data("engineered features")
